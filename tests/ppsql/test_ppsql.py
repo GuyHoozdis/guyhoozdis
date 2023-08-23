c7732726e1e3215bb85c090eff1809a09d0a33c7
Scott Sullivan <scott.sullivan@deepcellbio.com>
2023-08-22 21:18:13 -0500

    Mark tests as expected to fail

--- tests/ppsql/test_ppsql.py
+++ tests/ppsql/test_ppsql.py
@@ -1,10 +1,15 @@
+import pytest
+
+@pytest.mark.xfail(strict=True, reason="Not yet implemented.")
 def test_ppsql_requires_sqlparse():
     assert False
 
 
+@pytest.mark.xfail(strict=True, reason="Not yet implemented.")
 def test_ppsql_formats_sql_as_string():
     assert False
 
 
+@pytest.mark.xfail(strict=True, reason="Not yet implemented.")
 def test_ppsql_formats_sqlalchemy_seletable():
     assert False
